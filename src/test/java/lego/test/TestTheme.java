3f6b9777433d435c95235579761f40e7f0009c8a
Matthias Colin <matthias.colin@gmail.com>
2019-09-11 12:05:03 +0200

    add J11 version and TestTheme

---
+++ src/test/java/lego/test/TestTheme.java
@@ -0,0 +1,14 @@
+package lego.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestTheme {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
